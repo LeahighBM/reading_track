ef703412c42c91a86b7a1fc64b34c3ba3a81078c
LeahighBM <bleahighmc@gmail.com>
2025-06-19 14:20:10 -0600

    finish up some wishlist things

--- FastAPI/models.py
+++ FastAPI/models.py
@@ -10,3 +10,13 @@ class Wishlist(Base):
     author = Column(String)
     date_added = Column(String)
     already_own = Column(Boolean)
+
+class Completed(Base):
+    __tablename__ = "complete_books"
+
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    title = Column(String)
+    author = Column(String)
+    num_pages = Column(Integer)
+    date_started = Column(String)
+    date_completed = Column(String)
